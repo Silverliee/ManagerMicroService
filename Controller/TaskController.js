6c7c1275b013a47817cdd6630f53c8a4ba30e577
christopheylp <christopheyalap957@gmail.com>
2021-06-16 01:05:01 +0200

    Added routes for Task entity

--- Controller/TaskController.js
+++ Controller/TaskController.js
@@ -11,7 +11,28 @@ router.get('/', async (req, res) => {
 
 //recupere les tasks assigner à l'utilisateur grâce a son id
 router.get('/user/:id', async (req, res) => {
-    const projectWithUserTask = await taskModel.find({assigned_user_id: req.params.id})
+    await taskModel.find({assigned_user_id: req.params.id})
+        .then(products => res.status(200).json(products))
+        .catch(error => res.status(404).json({error}));
+});
+
+//recupere les tasks CREATED assigner à l'utilisateur grâce a son id
+router.get('/user/created/:id', async (req, res) => {
+    await taskModel.find({assigned_user_id: req.params.id, state:"CREATED"})
+        .then(products => res.status(200).json(products))
+        .catch(error => res.status(404).json({error}));
+});
+
+//recupere les tasks IN PROGRESS assigner à l'utilisateur grâce a son id
+router.get('/user/inprogress/:id', async (req, res) => {
+    await taskModel.find({assigned_user_id: req.params.id, state:"IN PROGRESS"})
+        .then(products => res.status(200).json(products))
+        .catch(error => res.status(404).json({error}));
+});
+
+//recupere les tasks FINISH assigner à l'utilisateur grâce a son id
+router.get('/user/finish/:id', async (req, res) => {
+    await taskModel.find({assigned_user_id: req.params.id, state:"FINISH"})
         .then(products => res.status(200).json(products))
         .catch(error => res.status(404).json({error}));
 });
@@ -23,6 +44,20 @@ router.post('/', async(req, res) => {
     });
     await task.save()
         .then(() => res.status(201).json({message: "La tache à été créer !"}))
+        .catch(error => res.status(400).json({error}));
+});
+
+// Put resquest Here
+router.put('/:id', async(req, res) => {
+    await taskModel.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(() => res.status(201).json({message: "Le projet a bien été modifié"}))
+        .catch(error => res.status(400).json({error}));
+});
+
+//Delete Request here
+router.delete('/:id', async(req, res) => {
+    await projectModel.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json({message: "Le projet a bien été supprimé"}))
         .catch(error => res.status(400).json({error}));
 });
 
