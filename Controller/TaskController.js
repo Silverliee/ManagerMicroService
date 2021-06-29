231551d9a3bc7c125a7c59a53e0b28e2c26b1330
christopheylp <christopheyalap957@gmail.com>
2021-06-30 00:36:03 +0200

    Added route who return deadline state

--- Controller/TaskController.js
+++ Controller/TaskController.js
@@ -23,29 +23,39 @@ router.get('/project/:id', async (req, res) => {
         .catch(error => res.status(404).json({error}));
 });
 
+//return true ou false si la deadline est passée ou non
+router.get('/deadline/:id', async (req, res) => {
+    const task = await taskModel.findById(req.params.id);
+    const dateTask = new Date(task.deadline);
+    console.log(dateTask)
+    const dateDay = new Date();
+    console.log(dateDay)
+    return res.status(201).json(dateDay < dateTask);
+});
+
 //recupere les tasks CREATED assigner à l'utilisateur grâce a son id
 router.get('/user/created/:id', async (req, res) => {
-    await taskModel.find({assigned_user_id: req.params.id, state:"CREATED"})
+    await taskModel.find({assigned_user_id: req.params.id, state: "CREATED"})
         .then(products => res.status(200).json(products))
         .catch(error => res.status(404).json({error}));
 });
 
 //recupere les tasks IN PROGRESS assigner à l'utilisateur grâce a son id
 router.get('/user/inprogress/:id', async (req, res) => {
-    await taskModel.find({assigned_user_id: req.params.id, state:"IN PROGRESS"})
+    await taskModel.find({assigned_user_id: req.params.id, state: "IN PROGRESS"})
         .then(products => res.status(200).json(products))
         .catch(error => res.status(404).json({error}));
 });
 
 //recupere les tasks FINISH assigner à l'utilisateur grâce a son id
 router.get('/user/finish/:id', async (req, res) => {
-    await taskModel.find({assigned_user_id: req.params.id, state:"FINISH"})
+    await taskModel.find({assigned_user_id: req.params.id, state: "FINISH"})
         .then(products => res.status(200).json(products))
         .catch(error => res.status(404).json({error}));
 });
 
 // POST Request Here
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
     const task = new taskModel({
         ...req.body
     });
@@ -55,33 +65,33 @@ router.post('/', async(req, res) => {
 });
 
 // Put resquest Here
-router.put('/:id', async(req, res) => {
+router.put('/:id', async (req, res) => {
     await taskModel.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
         .then(() => res.status(201).json({message: "La tache a bien été modifié"}))
         .catch(error => res.status(400).json({error}));
 });
 
-router.get('/assigned/:id/:userid', async(req, res) => {
-    await taskModel.updateOne({_id: req.params.id}, {assigned_user_id:req.params.userid})
+router.get('/assigned/:id/:userid', async (req, res) => {
+    await taskModel.updateOne({_id: req.params.id}, {assigned_user_id: req.params.userid})
         .then(() => res.status(201).json({message: "La tache a bien été modifié"}))
         .catch(error => res.status(400).json({error}));
 });
 
-router.get('/:id/inprogress', async(req, res) => {
-    await taskModel.updateOne({_id: req.params.id}, {state:"IN PROGRESS", _id: req.params.id})
+router.get('/:id/inprogress', async (req, res) => {
+    await taskModel.updateOne({_id: req.params.id}, {state: "IN PROGRESS", _id: req.params.id})
         .then(() => res.status(201).json({message: "La tache a bien été modifié"}))
         .catch(error => res.status(400).json({error}));
 });
 
-router.get('/:id/finish', async(req, res) => {
-    await taskModel.updateOne({_id: req.params.id}, {state:"FINISH", _id: req.params.id})
+router.get('/:id/finish', async (req, res) => {
+    await taskModel.updateOne({_id: req.params.id}, {state: "FINISH", _id: req.params.id})
         .then(() => res.status(201).json({message: "La tache a bien été modifié"}))
         .catch(error => res.status(400).json({error}));
 });
 
 
 //Delete Request here
-router.delete('/:id', async(req, res) => {
+router.delete('/:id', async (req, res) => {
     await projectModel.deleteOne({_id: req.params.id})
         .then(() => res.status(200).json({message: "Le projet a bien été supprimé"}))
         .catch(error => res.status(400).json({error}));
